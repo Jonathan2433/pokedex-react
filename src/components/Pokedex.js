ab19b3868edb47329ba99ade57bc7468e655c74a
Jonathan <jonathan.dupau24@gmail.com>
2021-03-13 19:34:19 +0100

    fix axios bug on chrome and add styles

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,24 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Pokemon from './Pokemon';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Pokemon from "./Pokemon";
 
 const Pokedex = () => {
-    const [data, setData] = useState([""]);
+  const [data, setData] = useState([""]);
 
-    useEffect(() => {
-        axios
-        .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-        .then((res) =>
-            setData(res.data.results));
-    }, []);
-    return (
-        <section id="pokedex">
-            { data.length > 0 && data.map((pokemon) => (     
-                <Pokemon key={pokemon.id} pokemon={pokemon} />         
-            ))}
-
-        </section>
-    );
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => setData(res.data.results));
+  }, []);
+  return (
+    <section id="pokedex">
+      { data.map((pokemon) => ( <Pokemon key={pokemon.name} pokemon={pokemon} />))}
+    </section>
+  );
 };
 
 export default Pokedex;
